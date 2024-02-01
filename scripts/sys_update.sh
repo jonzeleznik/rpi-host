b79e5a95eff2883e58db7ab4af1ec8882066ee4e
jonzeleznik <jon.zeleznik@gmail.com>
2024-02-01 23:22:40 +0100

    Add: check public IP

--- scripts/sys_update.sh
+++ scripts/sys_update.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 source .env
 
-sudo apt update && sudo apt upgrade -y \
-&& curl -H "Tags: heavy_check_mark" -H "Title: Successful" -u racunalnicec:trolololo -d "Sys updated" https://rem.cargoesbrrr.com/alerts \
-|| curl -H "Tags: x" -u "$NTFY_USER":"$NTFY_PASSWORD" -H "Title: Error" -d "Something went wrong when updating sys" https://rem.cargoesbrrr.com/alerts
+error=false
+error_msgs=()
+
+# --- update system ---
+if sudo apt update && sudo apt upgrade -y 
+then
+    error_msgs+=("")
+else
+    error=true
+    error_msgs+=("Something went wrong when updating sys")
+fi
+
+# --- check for ip change ---
+current_ip=$(curl ipinfo.io/ip)
+prev_ip=$(cat /home/pi/rpi-host/scripts/.ip)
+
+if [[ "$current_ip" != "$prev_ip" ]]
+then
+	echo $current_ip > .ip
+    error=true
+    error_msgs+=("Your public IP has changed!")
+fi
+
+# TODO: docker container status
+
+# --- check for errors ---
+if $error 
+then
+    msg=$(printf "|%s|" "${error_msgs[@]}")
+    curl -H "Tags: x" -u "$NTFY_USER":"$NTFY_PASSWORD" -H "At: 7am" -H "Title: Error" -d "$msg" https://rem.cargoesbrrr.com/alerts
+else
+    curl -H "Tags: heavy_check_mark" -H "At: 7am" -H "Title: Successful" -u "$NTFY_USER":"$NTFY_PASSWORD" -d "Sys updated" https://rem.cargoesbrrr.com/alerts
+fi
