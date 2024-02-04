86aa74016b9b1c84c95e2ab88afe316b86381ccb
jonzeleznik <jon.zeleznik@gmail.com>
2024-02-04 18:58:24 +0100

    Add: docker script, static path

--- scripts/sys_update.sh
+++ scripts/sys_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env
+source /home/pi/rpi-host/scripts/.env
 
 error=false
 error_msgs=()
@@ -19,18 +19,49 @@ prev_ip=$(cat /home/pi/rpi-host/scripts/.ip)
 
 if [[ "$current_ip" != "$prev_ip" ]]
 then
-	echo $current_ip > .ip
+	echo $current_ip > /home/pi/rpi-host/scripts/.ip
     error=true
     error_msgs+=("Your public IP has changed!")
 fi
 
-# TODO: docker container status
+containers=$(docker ps -a --format "{{.ID}}")
+
+sep='-'
+declare -a container_info=()
+
+if [ -z "$containers" ]
+then
+  echo "No Docker containers found."
+else
+  # Loop through each container and store its name and status in the array
+  for container_id in $containers
+  do
+    container_status=$(docker inspect -f '{{.State.Status}}' "$container_id")
+
+    if [ $container_status == "exited" ]
+    then
+      container_name=$(docker inspect -f '{{.Name}}' "$container_id" | sed 's/\///')
+      container_info+=("${container_name}")
+    fi
+  done
+fi
+
+docker=""
+for i in "${!container_info[@]}"
+do
+  docker+=$(printf '%s | ' "${container_info[$i]}")
+done
+
 
 # --- check for errors ---
 if $error 
 then
     msg=$(printf "|%s|" "${error_msgs[@]}")
-    curl -H "Tags: x" -u "$NTFY_USER":"$NTFY_PASSWORD" -H "At: 7am" -H "Title: Error" -d "$msg" https://rem.cargoesbrrr.com/alerts
+    curl -u "$NTFY_USER":"$NTFY_PASSWORD" -H "Tags: x" -H "At: 7am" -H "Title: Error" -d "$msg
+    Docker Containers
+    $docker" https://rem.cargoesbrrr.com/alerts
 else
-    curl -H "Tags: heavy_check_mark" -H "At: 7am" -H "Title: Successful" -u "$NTFY_USER":"$NTFY_PASSWORD" -d "Sys updated" https://rem.cargoesbrrr.com/alerts
+    curl -u "$NTFY_USER":"$NTFY_PASSWORD" -H "Tags: heavy_check_mark" -H "At: 7am" -H "Title: Successful" -d "Sys updated
+    Docker Containers
+    $docker" https://rem.cargoesbrrr.com/alerts
 fi
